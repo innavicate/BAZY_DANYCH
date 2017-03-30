ecc7d10feab459ebe7bf679864a494745d2c5cfa
innavicate <xvalerinex@gmail.com>
2017-03-30 18:16:05 +0200

    Add files via upload

---
+++ insert_ulica.sql
@@ -0,0 +1,23 @@
+INSERT INTO Ulica VALUES(1,'Polna',7);
+INSERT INTO Ulica VALUES(2,'Kwiatowa',15);
+INSERT INTO Ulica VALUES(3,'Spokojna',13);
+INSERT INTO Ulica VALUES(4,'Mila',5);
+INSERT INTO Ulica VALUES(5,'Wesola',2);
+INSERT INTO Ulica VALUES(6,'Klonowa',1);
+INSERT INTO Ulica VALUES(7,'Cicha',1);
+INSERT INTO Ulica VALUES(8,'Jana Pawla II',56);
+INSERT INTO Ulica VALUES(9,'Waska',3);
+INSERT INTO Ulica VALUES(10,'Boczna',12);
+INSERT INTO Ulica VALUES(11,'Zielona',7);
+INSERT INTO Ulica VALUES(12,'Grochowa',25);
+INSERT INTO Ulica VALUES(13,'Sienkiewicza',32);
+INSERT INTO Ulica VALUES(14,'Szara',11);
+INSERT INTO Ulica VALUES(15,'Morska',49);
+INSERT INTO Ulica VALUES(16,'Kolataja',3);
+INSERT INTO Ulica VALUES(17,'Mickiewicza',55);
+INSERT INTO Ulica VALUES(18,'Paderewskiego',23);
+INSERT INTO Ulica VALUES(19,'Sezamona',11);
+INSERT INTO Ulica VALUES(20,'Bakowa',99);
+
+
+
